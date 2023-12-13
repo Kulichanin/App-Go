1459150c25b68672793ffc2ee4fa6bbde1d8f41e
Dmitry <mr.gn0m@yandex.ru>
2023-12-13 20:40:14 +0300

    create new code

--- app.go
+++ app.go
@@ -1,43 +1,24 @@
-// !TODO Проправить запуск из крона или сервиса
 package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 )
 
-func read_html(name_file string) string {
-	file, err := os.Open(name_file)
-	_ = err
-	defer file.Close()
-
-	// получить размер файла
-	stat, err := file.Stat()
-	_ = err
-
-	// чтение файла
-	bs := make([]byte, stat.Size())
-	_, err = file.Read(bs)
-	_ = err
-
-	return string(bs)
-}
-
-func hello(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set(
-		"Content-Type",
-		"text/html",
-	)
-	io.WriteString(
-		res,
-		read_html("index.html"),
-	)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello World!</h1>"))
 }
 
 func main() {
-	fmt.Print("Server start!")
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":3000", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	fmt.Print("Server start! Port :3000")
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
 }
